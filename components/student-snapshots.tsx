37331876db26e4fff7e787402e02569ac4c4001c
Nehil <chnadrakarnehil.06112002@gmail.com>
2026-01-12 20:43:59 +0530

    fix: Video issue fixed for vercel

--- components/student-snapshots.tsx
+++ components/student-snapshots.tsx
@@ -26,7 +26,7 @@ export default function StudentSnapshots() {
           poster="/images/video-poster.jpg"
         >
           {/* Add your video source here */}
-          <source src="/videos/download.MP4" type="video/mp4" />
+          <source src="/videos/download.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
